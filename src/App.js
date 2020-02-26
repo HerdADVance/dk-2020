03b2bb0841e8a35dfaec07689d10e9d8bdd7623c
Alex <herdadvance@gmail.com>
2020-02-26 00:58:24 -0500

    Dependencies and basic setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
+import { Link } from "@reach/router";
+
+const App = () => {
+  return (
+    <h1>DK Lineup Builder</h1>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
